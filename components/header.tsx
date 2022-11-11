48b3ed512f37c9a4ac7c588d9059439a6d46bb14
Caio Fresneda <caiofresneda@gmail.com>
2022-11-11 03:47:06 -0300

    header

---
+++ components/header.tsx
@@ -0,0 +1,13 @@
+import react, { useMemo } from 'react';
+import icons from '../assets/icons';
+
+
+const Header = () => {
+    return <>
+    <header>
+        <icons.Logo />
+    </header>
+    </>
+}
+
+export default Header;
