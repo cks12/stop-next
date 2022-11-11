662a2a82d95d3073b14c9ec1ffbe24b0110334cc
Caio Fresneda <caiofresneda@gmail.com>
2022-11-11 03:47:49 -0300

    add typ

---
+++ type.d.ts
@@ -0,0 +1,23 @@
+export interface applicationInterface {
+    name: string;
+    isReady:boolean;
+    players:player[];
+    // rooms: roomInformation[],
+    selectedRoom: roomInformation,
+    gameStart:boolean,
+    host:boolean
+}
+
+export interface roomInformation {
+    name: string;
+    id: string;
+    playerNumber: number;
+    limit: number;
+}
+
+export interface player {
+    ready: boolean,
+    name: string,
+    isReady: boolean,
+    gameStart: boolean,
+}
