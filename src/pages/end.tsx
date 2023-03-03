351589b74fd4cb15e5fddb934f0d93706322b284
Caio Caik Fresneda de Souza <caiofresneda@gmail.com>
2023-03-03 02:30:33 -0300

    Add title end page

--- src/pages/end.tsx
+++ src/pages/end.tsx
@@ -2,6 +2,7 @@ import Button from "components/button";
 import Table from "components/table";
 import { ApplicationProps, playerFinished } from "types";
 import Router from 'next/router';
+import Head from "next/head";
 
 interface Props {
   players: playerFinished[];
@@ -12,9 +13,14 @@ const PlayersPage: React.FC<ApplicationProps> = ({Application}) => {
     console.log(players.map(p=>(p.fields)));
     async function home(){
       await Router.push("/");
-      
     }
   return (
+    <>
+    
+    <Head>
+      <title>Final Game</title>
+    </Head>
+
     <div className="px-2 w-screen flex justify-center flex-col items-center">
       <div className="md:max-w-xl bg-[rgba(0,0,0,0.1)] shadow-lg px-2 md:px-10 py-2 max-w-2xl w-[99%] rounded-[20px]">
 
@@ -26,6 +32,7 @@ const PlayersPage: React.FC<ApplicationProps> = ({Application}) => {
       </div>
       <Button onClick={home} className="bg-green-500 hover:bg-green-700 mt-10">Novo Jogo</Button>
     </div>
+      </>
   );
 };
 
